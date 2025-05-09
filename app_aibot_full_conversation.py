9728e75ce90a83aa08db7e7f9f04b4b3dd5f4dae
VyLuong94 <thuyvy.luong942002@gmail.com>
2025-05-09 07:58:34 +0000

    Update app code

--- app_aibot_full_conversation.py
+++ app_aibot_full_conversation.py
@@ -923,7 +923,7 @@ def main():
                         transcript,
                         uploaded_excel_file,
                         method="rag",
-                        threshold=0.6
+                        threshold=0.4
                     )
 
                     st.subheader("Tỷ lệ tuân thủ tổng thể:")
