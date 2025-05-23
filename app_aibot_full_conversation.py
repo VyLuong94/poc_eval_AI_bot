4f05c424505fed9caa533f8710ff34bbfd2b5c51
VyLuong94 <thuyvy.luong942002@gmail.com>
2025-05-23 03:32:50 +0000

    Update app code

--- app_aibot_full_conversation.py
+++ app_aibot_full_conversation.py
@@ -1215,7 +1215,7 @@ def main():
                         df_sop_results["Trạng thái"] = df_sop_results["Trạng thái"].apply(
                             lambda x: "Y" if str(x).strip().lower() == "đã tuân thủ" else "N"
                         )
-                        sheet_name_detected = detected_sheets_by_file.get(file_name, "").strip().lower()
+                        sheet_name_detected = detected_sheets_by_file.get(file_name, "").strip()
                         if "NT" in sheet_name_detected:
                             call_type = "NT"
                         elif "KH" in sheet_name_detected:
